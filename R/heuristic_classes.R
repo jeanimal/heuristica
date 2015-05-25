7f787df67b60d592af3261209f26f1c89023c5f2
jeanwhitmore <jeanimal@gmail.com>
2015-05-25 11:45:59 -0500

    Finally got the generic predict function working in the recommended way with ...

--- R/heuristic_classes.R
+++ R/heuristic_classes.R
@@ -1,28 +1,34 @@
 #' Take The Best
 #'
-#' An S3 class implementation of the Take The Best heuristic.
-#' It sorts cues in order of cue validity, making a decision based on the first cue that
+#' An implementation of the Take The Best heuristic.
+#' It sorts cues in order of \code{\link{cueValidity}}, making a decision based on the first cue that
 #' discriminates (has differing values on the two objects).
-#' Implements:
-#'   predict
-#' However, its implementation of the S3 predict generic function has to output values.
-#' Do not take the actual values seriously-- only the sort order is guaranteed. 
 #' 
 #' @param train_data Training/fitting data as a matrix or data.frame.
 #' @param criterion_col The index of the colum in train_data that has the criterion.
 #' @param cols_to_fit A vector of column indices in train_data.
 #'
-#' @return The ttb S3 object.
+#' @return An object of \code{\link[base]{class}} ttbModel.  This is a list containing at least the following components:
+#'   \itemize{
+#'    \item "cue_validities": A list of cue validities for the cues in order of cols_to_fit.
+#'    \item "cue_ranks": A list of integer ranks of the cues in order of cols_to_fit.
+#'                       The cue ranked 1 will be used first, then 2, etc.
+#'   }
 #'
 #' @examples
 #' ## Fit column (5,4) to column (1,0), having validity 1.0, and column (0,1), validity 0.
 #' ttb <- ttbModel(matrix(c(5,4,1,0,0,1), 2, 3), 1, c(2,3))
-#' ## This outputs predicted values for the first and second values, but only the sort order is guaranteed.
-#' ## so this is considered a "correct" prediction.
+#' ## Outputs predicted values for the first and second values, but Take The Best
+#' ## is only trying to achieve sort order, so this makes a correct prediction. 
 #' predict(ttb, matrix(c(5,4,1,0,0,1), 2, 3)) 
+#' ## But this input results in an incorrect prediction:
+#' predict(ttb, matrix(c(5,4,0,1,0,1), 2, 3)) 
 #'
 #' @seealso
 #' \code{\link{predict.ttbModel}} (via \code{\link[stats]{predict}}) for prediction.
+#' @seealso
+#' Wikipedia's entry on \url{http://en.wikipedia.org/wiki/Take-the-best_heuristic}.
+#'
 #' @export
 ttbModel <- function(train_data, criterion_col, cols_to_fit) {
   cue_validities <- matrixCueValidity(train_data[,c(criterion_col,cols_to_fit)], criterion_col)
@@ -37,6 +43,22 @@ coef.ttbModel <- function(model) model$linear_coef
 #' Generates predictions for Take The Best
 #'
 #' Implementation of \code{\link[stats]{predict}} for ttbModel.
+#'
+#' @param model A ttbModel.
+#'  newdata is used to predict and can be a matrix or data.frame.  
+#'  It must have the same cols_to_fit indices as those used in train_data.
+#'
+#' @return An N x 1 matrix of predicted values, or a list if there was only one cue.
+#'  Only the sort order of these is relevant for Take The Best.
+#' 
 #' @export
-predict.ttbModel <- function(model, test_df) predictWithWeights(test_df, model$cols_to_fit, model$linear_coef)
+# Under the hood, TTB is implemented as a linear prediction with exponentiall-decaying weights.
+# This is just because it was more convenient to code it that way in R.
+# The output is equivalent to the model description.
+predict.ttbModel <- function(model, ...) {
+  args <- eval(substitute(alist(...)))
+  #TODO: Make this handle missing test_dta.
+  test_data <- eval(args[[1]])
+  predictWithWeights(test_data, model$cols_to_fit, model$linear_coef)
+}
 
