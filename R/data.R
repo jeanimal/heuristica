e5349fe24a1930d63998b63c28780c4471d2c923
jeanwhitmore <jeanimal@gmail.com>
2015-08-15 11:44:36 -0500

    Added high school dropout data.  I think the column names are pretty self-explanatory, but maybe I will document them more.

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Chicago high school dropout rates. 
+#'
+#' Chicago high school droput rates from 1995 and associated variables
+#' like average students per teach and percent low income.
+#'
+#' The data is based on:
+#'
+#'  Morton, Felicia B. (1995). Charting a School¹s Course. Chicago.
+#'     February, pp. 86-95.
+#'
+#'  Rodkin, Dennis. (1995). 10 Keys for Creating Top High Schools.
+#'     Chicago. February, pp. 78-85.
+#'
+#' There are 18 data columns.  The data.frame is
+#' larger because it has extra info. 
+#'
+#' This is the data set used in simulations by the ABC (Adaptive Behavior
+#' and Cognition) research group.
+#'
+#' @format A data frame.
+#' \describe{
+#'   \item{Name_of_School}{Name of school}
+#'   ...
+#' }
+"abc_hs_dropout"
