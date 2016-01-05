b2ad08fc860106c02e0aeda43c7c6da89e093e7c
jeanwhitmore <jeanimal@gmail.com>
2016-01-05 07:50:18 -0600

    Migrated test-row_pair_apply to more readable matrix and data.frame construtors

--- tests/testthat/test-row_pair_apply.R
+++ tests/testthat/test-row_pair_apply.R
@@ -8,7 +8,7 @@ context("row_pair_apply")
 
 #TODO: Do this test with a dumber function than ttb.
 test_that("allRowPairApply dimension test: matrix, 2 rows = 1 pair", {
-  train_matrix <- matrix(c(5,4,1,0,0,1), 2, 3)
+  train_matrix <- cbind(y=c(5,4), x1=c(1,0), x2=c(0,1))
   model <- ttbModel(train_matrix, 1, c(2,3))
   out1 <- allRowPairApply(train_matrix, heuristics(model))
   expect_equal(1, nrow(out1))
@@ -19,7 +19,7 @@ test_that("allRowPairApply dimension test: matrix, 2 rows = 1 pair", {
 })
 
 test_that("allRowPairApply dimension test: matrix, 3 rows = 3 pairs", {
-  train_df <- data.frame(matrix(c(5,4,3,1,0,0,0,1,0), 3, 3))
+  train_df <- data.frame(y=c(5,4,3), x1=c(1,0,0), x2=c(0,1,0))
   model <- ttbModel(train_df, 1, c(2,3))
   out1 <- allRowPairApply(train_df, heuristics(model))
   expect_equal(3, nrow(out1))
@@ -30,7 +30,7 @@ test_that("allRowPairApply dimension test: matrix, 3 rows = 3 pairs", {
 })
 
 test_that("allRowPairApply dimension test: data.frame", {
-  train_df <- data.frame(matrix(c(5,4,3,1,0,0,0,1,0), 3, 3))
+  train_df <- data.frame(y=c(5,4,3), x1=c(1,0,0), x2=c(0,1,0))
   model <- ttbModel(train_df, 1, c(2,3))
   out1 <- allRowPairApply(train_df, heuristics(model))
   expect_equal(3, nrow(out1))
@@ -41,7 +41,7 @@ test_that("allRowPairApply dimension test: data.frame", {
 })
 
 test_that("allRowPairApply dimension test: heuristics, heuristics", {
-  train_df <- data.frame(matrix(c(5,4,3,1,0,0,0,1,0), 3, 3))
+  train_df <- data.frame(y=c(5,4,3), x1=c(1,0,0), x2=c(0,1,0))
   model <- ttbModel(train_df, 1, c(2,3))
   out1 <- allRowPairApply(train_df, heuristics(model))
   expect_equal(3, nrow(out1))
@@ -53,7 +53,7 @@ test_that("allRowPairApply dimension test: heuristics, heuristics", {
 
 # Test criterion function
 test_that("allRowPairApply criterion function", {
-  train_df <- data.frame(matrix(c(5,4,3,1,0,0,0,1,0), 3, 3))
+  train_df <- data.frame(y=c(5,4,3), x1=c(1,0,0), x2=c(0,1,0))
   model <- ttbModel(train_df, 1, c(2,3))
   out1 <- allRowPairApply(train_df, criterion(1))
   expect_equal(3, nrow(out1))
@@ -65,7 +65,7 @@ test_that("allRowPairApply criterion function", {
 
 # Test colPairValues function
 test_that("allRowPairApply colPairValues function numeric", {
-  train_df <- data.frame(matrix(c(5,4,3,1,0,0,0,1,0), 3, 3))
+  train_df <- data.frame(y=c(5,4,3), x1=c(1,0,0), x2=c(0,1,0))
   model <- ttbModel(train_df, 1, c(2,3))
   # colPairValues outputs 2 columns
   out1 <- allRowPairApply(train_df, colPairValues(1, "a"))
