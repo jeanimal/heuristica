4de48cdc1bb9e1716e46c545ada37a9c51ce1b7a
jeanwhitmore <jeanimal@gmail.com>
2016-02-16 07:54:15 -0600

    data.R all lines < 80 chars

--- R/data.R
+++ R/data.R
@@ -18,16 +18,28 @@
 #' \describe{
 #'   \item{Name_of_School}{Name of School}
 #'   \item{Running_Number}{Running Number}
-#'   \item{Included_in_Web_and_Web_Corrected}{If 1, then this row was accidentally omitted in the ABC studies from 1993}
-#'   \item{Dropout_Rate}{Dropout rate in percent, from 0 to 100, counting all students in grades 9 through 12 who left school permanently during the 1993-4 school year}
+#'   \item{Included_in_Web_and_Web_Corrected}{If 1, then this row was
+#'     accidentally omitted in the ABC studies from 1993}
+#'   \item{Dropout_Rate}{Dropout rate in percent, from 0 to 100, counting all
+#'     students in grades 9 through 12 who left school permanently during the
+#'     1993-4 school year}
 #'   \item{Completeness_of_Data}{Completeness of data}
 #'   \item{Enrollment}{Enrollment as of September 30, 1993}
-#'   \item{Attendance_Rate}{Attendance rate in percent, from 0 to 100, averaged over the school year}
-#'   \item{Graduation_Rate}{Graduation rate in percent, from 0 to 100, based on freshmen who finished together 4 years later, in 1994}
-#'   \item{Parental_Involvement_Rate}{Parental involvement rate in percent, from 0 to 100, counted as parents who had contact with teachers as a percent of students (with no firm state rules on how to measure this)}
-#'   \item{Limited_English_Students}{Limited English Students in percent, from 0 to 100, based on the number of students found eligible for bilingual education}
-#'   \item{Low_Income_Students}{Low Income Students in percent, from 0 to 100, based on families eligible for free or reduced price lunches or are publicly supported}
-#'   \item{Average_Class_Size_Student_per_Teacher}{Calculated as number of students divided by number of teachers on the first day of May}
+#'   \item{Attendance_Rate}{Attendance rate in percent, from 0 to 100, averaged
+#'     over the school year}
+#'   \item{Graduation_Rate}{Graduation rate in percent, from 0 to 100, based on
+#'     freshmen who finished together 4 years later, in 1994}
+#'   \item{Parental_Involvement_Rate}{Parental involvement rate in percent,
+#'     from 0 to 100, counted as parents who had contact with teachers as a
+#'     percent of students (with no firm state rules on how to measure this)}
+#'   \item{Limited_English_Students}{Limited English Students in percent, from
+#'     0 to 100, based on the number of students found eligible for bilingual
+#'     education}
+#'   \item{Low_Income_Students}{Low Income Students in percent, from 0 to 100,
+#'     based on families eligible for free or reduced price lunches or are
+#'     publicly supported}
+#'   \item{Average_Class_Size_Student_per_Teacher}{Calculated as number of
+#'     students divided by number of teachers on the first day of May}
 #'   \item{Percent_White_Students}{Percent white students, from 0 to 100}
 #'   \item{Percent_Black_Students}{Percent black students, from 0 to 100}
 #'   \item{Percent_Hispanic_Students}{Percent hispanic students, from 0 to 100}
@@ -46,11 +58,13 @@
 #' Population size of the 83 largest German cities. 
 #'
 #' Population size of 83 largest German cities and additional variables 
-#' indicating whether they have a soccer team, intercity trainline, University, etc.
+#' indicating whether they have a soccer team, intercity trainline,
+#' University, etc.
 #'
 #' The data is based on:
 #'
-#'  Fischer Welt Almanach [Fischer World Almanac]. (1993).  Frankfurt, Germany: Fischer.
+#'  Fischer Welt Almanach [Fischer World Almanac]. (1993).  Frankfurt,
+#'    Germany: Fischer.
 #'
 #'
 #' This is the data set used in simulations by the ABC (Adaptive Behavior
@@ -61,14 +75,23 @@
 #'   \item{Name_of_City}{Name of city}
 #'   \item{Running_Number}{Running Number}
 #'   \item{Population}{Population size}
-#'   \item{Soccer_Team}{1 indicates that the city has a soccer team, 0 indicates that it does not.}
-#'   \item{State_Capital}{1 indicates that the city is a state capital, 0 indicates that it is not.}
-#'   \item{Former_East_Germany}{1 indicates that the city belongs to former East Germany, 0 that is does not.}
-#'   \item{Industrial_Belt}{1 indicates that the city is an industrial belt, 0 that it is not.}
-#'   \item{Licence_Plate}{1 indicates that the city has a licence plate, 0 that it does not.}
-#'   \item{Intercity_Trainline}{1 indicates that an intercity trainline crosses the city, 0 that it does not.}
-#'   \item{Exposition_Site}{1 indicates that the city is an exposition size, 0 that it is not.}
-#'   \item{National_Capital}{1 indicates that the city is the national capital, 0 that it is not.}
-#'   \item{University}{1 indicates that the city has a University, 0 that it does not.}
+#'   \item{Soccer_Team}{1 indicates that the city has a soccer team, 0
+#'     indicates that it does not.}
+#'   \item{State_Capital}{1 indicates that the city is a state capital, 0
+#'     indicates that it is not.}
+#'   \item{Former_East_Germany}{1 indicates that the city belongs to former
+#'     East Germany, 0 that is does not.}
+#'   \item{Industrial_Belt}{1 indicates that the city is an industrial belt,
+#'     0 that it is not.}
+#'   \item{Licence_Plate}{1 indicates that the city has a licence plate, 0
+#'     that it does not.}
+#'   \item{Intercity_Trainline}{1 indicates that an intercity trainline crosses
+#'     the city, 0 that it does not.}
+#'   \item{Exposition_Site}{1 indicates that the city is an exposition size, 0
+#'     that it is not.}
+#'   \item{National_Capital}{1 indicates that the city is the national capital,
+#'     0 that it is not.}
+#'   \item{University}{1 indicates that the city has a University, 0 that it
+#'     does not.}
 #' }
 "city_population"
