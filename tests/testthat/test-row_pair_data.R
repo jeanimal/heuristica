c56285abf2e481ff4a7e6303d038b0279aaa8fbf
jeanwhitmore <jeanimal@gmail.com>
2016-02-19 20:54:48 -0600

    Using toRowPairData directly.  No need for logRegData wrapper.

--- tests/testthat/test-row_pair_data.R
+++ tests/testthat/test-row_pair_data.R
@@ -1,31 +1,31 @@
 context("row_pair_data")
 
 
-test_that("logRegData matrix forward", {
+test_that("toRowPairData matrix forward", {
   train_data <- cbind(y=c(9,4), x1=c(3,0), x2=c(1,1))
-  out <- logRegData(train_data, 1, c(2,3), rowDiff)
+  out <- toRowPairData(train_data, 1, c(2,3), rowDiff)
   # The criterion is scaled to 0-1.
   expect_equal(cbind(y=c(1), x1=c(3), x2=c(0)), out)
 })
 
-test_that("logRegData matrix backward", {
+test_that("toRowPairData matrix backward", {
   # Reverse rows relative to prior test.
   train_data <- cbind(y=c(4,9), x1=c(0,3), x2=c(1,1))
-  out <- logRegData(train_data, 1, c(2,3), rowDiff)
+  out <- toRowPairData(train_data, 1, c(2,3), rowDiff)
   # The criterion is scaled to 0-1.
   expect_equal(cbind(y=c(0), x1=c(-3), x2=c(0)), out)
 })
 
-test_that("logRegData data.frame forward", {
+test_that("toRowPairData data.frame forward", {
   train_data <- data.frame(y=c(9,4), x1=c(3,0), x2=c(1,1))
-  out <- logRegData(train_data, 1, c(2,3), rowDiff)
+  out <- toRowPairData(train_data, 1, c(2,3), rowDiff)
   # The criterion is scaled to 0-1.
   expect_equal(cbind(y=c(1), x1=c(3), x2=c(0)), out)
 })
 
-test_that("logRegData matrix 3 row", {
+test_that("toRowPairData matrix 3 row", {
   train_data <- cbind(y=c(9,4,4), x1=c(3,0,0), x2=c(1,1,1))
-  out <- logRegData(train_data, 1, c(2,3), rowDiff)
+  out <- toRowPairData(train_data, 1, c(2,3), rowDiff)
   # Row 1 - row 2.
   expect_equal(cbind(y=c(1), x1=c(3), x2=c(0)), oneRow(out, 1))
   # Row 1 - row 3.
