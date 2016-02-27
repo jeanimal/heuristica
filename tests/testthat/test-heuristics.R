d4be230742392d04bb05003619c17c3ba621cbf3
jeanwhitmore <jeanimal@gmail.com>
2016-02-27 09:31:43 -0600

    Fixing tests that broke when I restarted my R session.  Global variables in R are annoying.

--- tests/testthat/test-heuristics.R
+++ tests/testthat/test-heuristics.R
@@ -873,21 +873,21 @@ test_that("logRegModel predictRowPair keepOrder", {
 # TODO: Move tests below to a new batch test file.
 
 test_that("logRegModel pctCorrectOfPredictPair easy 100%", {
-  df <- data.frame(Criterion=c(5,4,3,2,1), a1=c(5,4,3,2,1), a2=c(5,4,3,2,1))
+  df <- data.frame(Criterion=c(5,4,3,2,1), a=c(5,4,3,2,1))
   model <- logRegModel(df, 1, c(2))
-  out <- pctCorrectOfPredictPair(list(mod), df)
+  out <- pctCorrectOfPredictPair(list(model), df)
   expect_equal(1, out[, "logRegModel"])
 })
 
 test_that("logRegModel pctCorrectOfPredictPair cue reverse easy 100%", {
-  df <- data.frame(Criterion=c(5,4,3,2,1), a1=c(1,2,3,4,5), a2=c(5,4,3,2,1))
+  df <- data.frame(Criterion=c(5,4,3,2,1), a=c(1,2,3,4,5))
   model <- logRegModel(df, 1, c(2))
   out <- pctCorrectOfPredictPair(list(model), df)
   expect_equal(1, out[, "logRegModel"])
 })
 
 test_that("logRegModel pctCorrectOfPredictPair criterion reverse easy 100%", {
-  df <- data.frame(Criterion=c(1,2,3,4,5), a2=c(1,2,3,4,5), a2=c(5,4,3,2,1))
+  df <- data.frame(Criterion=c(1,2,3,4,5), a=c(5,4,3,2,1))
   model <- logRegModel(df, 1, c(2))
   out <- pctCorrectOfPredictPair(list(model), df)
   expect_equal(1, out[, "logRegModel"])
