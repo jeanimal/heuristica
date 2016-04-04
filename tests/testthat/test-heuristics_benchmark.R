b434652b581bbfdfc3bafba55cec0160aa1b51e9
jeanwhitmore <jeanimal@gmail.com>
2016-04-04 08:37:17 -0500

    Added benchmark test showing new simpleRowPairApply can be made fast enough.

--- tests/testthat/test-heuristics_benchmark.R
+++ tests/testthat/test-heuristics_benchmark.R
@@ -21,6 +21,18 @@ test_that("Benchmark ttbModel on city_population", {
   #   0.137   0.006   0.143
 })
 
+test_that("Benchmark ttbModel on city_population NEW simpleRowPairApply", {
+  ttb <- ttbModel(city_population, 3, c(4:ncol(city_population)))
+  times <- system.time(simpleRowPairApply(city_population,
+                                          createHeuristicWrapperFn2(ttb)))
+  print("ttb NEW simpleRowPairApply")
+  print(times)
+  
+  # On 11 Jan on Jean's macbook:
+  #    user  system elapsed
+  #   0.118   0.003   0.122
+})
+
 test_that("Benchmark regInterceptModel on city_population", {
   model <- regInterceptModel(city_population, 3, c(4:ncol(city_population)))
   times <- system.time(allRowPairApply(city_population, heuristics(model)))
