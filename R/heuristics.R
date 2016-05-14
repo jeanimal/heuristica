882d6398d3093cc6f1f9c089bff468aeb81fd755
jeanwhitmore <jeanimal@gmail.com>
2016-05-13 21:32:21 -0500

    Oops fit_name is now also in output of validityWeightedModel

--- R/heuristics.R
+++ R/heuristics.R
@@ -299,7 +299,8 @@ predictProbInternal.unitWeightModel <- function(object, row1, row2) {
 #'
 #' @export 
 validityWeightModel <- function(train_data, criterion_col, cols_to_fit,
-                                reverse_cues=TRUE, fit_name="validityWeightModel") {
+                                reverse_cues=TRUE,
+                                fit_name="validityWeightModel") {
   stopIfTrainingSetHasLessThanTwoRows(train_data)
   cv <- cueValidityMatrix(train_data, criterion_col, cols_to_fit,
                            reverse_cues=reverse_cues)
@@ -307,8 +308,8 @@ validityWeightModel <- function(train_data, criterion_col, cols_to_fit,
   linear_coef <- cv$cue_directions * cv$cue_validities
   structure(list(criterion_col = criterion_col, cols_to_fit = cols_to_fit,
                  cue_validities_unreversed=cv$cue_validities_unreversed,
-                 cue_validities=cv$cue_validities,
-                 linear_coef = linear_coef),
+                 cue_validities=cv$cue_validities, linear_coef = linear_coef
+                 fit_name=fit_name),
             class="validityWeightModel")
 }
 
