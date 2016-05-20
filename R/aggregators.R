33e387fb7aa73aa737fd60a87d91acf7e59f1657
jeanwhitmore <jeanimal@gmail.com>
2016-05-19 20:32:33 -0500

    Trying to remove the documentation that makes Travis break.

--- R/aggregators.R
+++ R/aggregators.R
@@ -78,21 +78,6 @@ aggregatePredictPair <- function(fitted_heuristic_list, test_data,
 #' df <- data.frame(y=c(30,20,10,5), name=c("a", "b", "c", "d"),
 #'                  x1=c(1,1,0,0), x2=c(1,1,0,1))
 #' ttb <- ttbModel(df, 1, c(3:4))
-#' sing <- singleCueModel(df, 1, c(3:4))
-#' pctCorrectOfPredictPair(list(ttb, sing), df)
-#' #    ttbModel singleCueModel
-#' #  1     0.75      0.8333333
-#' # TTB gets 75% correct while single cue model gets 83%.
-#'
-#' # Now repeatedly sample 2 rows of the data set and see how outcomes are
-#' # affected, tracking with the fit_name.
-#' set.seed(1) # If you want to reproduce the same output as below.
-#' ttb1 <- ttbModel(df[sample(nrow(df), 2),], 1, c(3:4), fit_name="fit1")
-#' ttb2 <- ttbModel(df[sample(nrow(df), 2),], 1, c(3:4), fit_name="fit2")
-#' ttb3 <- ttbModel(df[sample(nrow(df), 2),], 1, c(3:4), fit_name="fit3")
-#' pctCorrectOfPredictPair(list(ttb1, ttb2, ttb3), df)
-#' #        fit1 fit2 fit3
-#' # 1 0.8333333 0.75 0.75
 #'
 #' @seealso
 #'   using one fitted heuristic.
